afc19d53ac8988c184fb8bc4395ab1ba40312c6f
Artem Barmin <1barmin146@gmail.com>
2021-03-13 10:40:37 +0300

    create project

---
+++ LN/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LN
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SchoolEntities schoolEntities = new SchoolEntities();
+            Tabl.ItemsSource = schoolEntities.Client.ToList(); //выводим данные из таблицы
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed; 
+            Window1 window1 = new Window1();
+            window1.Show();//выводим форму добавления клиента
+        }
+
+        private void search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SchoolEntities schoolEntities = new SchoolEntities();
+            var i = from u in schoolEntities.Client
+                    where ((u.FirstName.Contains(search.Text)) || (u.LastName.Contains(search.Text)) || (u.Patronymic.Contains(search.Text)) || (u.Email.Contains(search.Text)))//ищем совпаденя в полях
+                    select new { u.FirstName, u.LastName, u.Patronymic, u.Email, u.Gender, u.Birthday, u.RegistrationDate };
+            Tabl.ItemsSource = i.ToList(); //выводим новые поля
+            kolvo.Text = i.Count().ToString();// выводим количество строк в таблице
+        }
+    }
+}
---
+++ LN/Window1.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LN
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void addClient_Click(object sender, RoutedEventArgs e)
+        {
+            string nName = NewName.Text;
+            string nLastName = NewLastName.Text;
+            string nPatr = NewPatr.Text;
+            DateTime nBirth = new DateTime();
+            DateTime nRD = new DateTime(); 
+            string nEmail = NewEmail.Text;
+            string nPhone = NewPhone.Text;
+            string nGendr = NewGendr.Text;
+
+            nBirth = Convert.ToDateTime(NewBirthday.Text);
+            nRD = Convert.ToDateTime(NewRegistrDate.Text);
+
+            SchoolEntities schoolEntities = new SchoolEntities();
+            
+            if ((nName != null) & (nLastName != null) & (nRD != null) & (nEmail != null) & (nPhone != null) & (nGendr != null ))
+            {
+                Client NClient = new Client
+                {
+                    FirstName = nName,
+                    LastName = nLastName,
+                    Patronymic = nPatr,
+                    Birthday = nBirth,
+                    RegistrationDate = nRD,
+                    Email = nEmail,
+                    Phone = nPhone,
+                    GenderCode = nGendr
+                };
+
+                schoolEntities.Client.Add(NClient);
+                schoolEntities.SaveChanges();
+
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Visibility = Visibility.Visible;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("заполните поля обозначенные знаком \"*\" ! ");
+            }
+        }
+    }
+}
